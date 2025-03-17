f9e91eb932b574cdfd405ba6febd624d26d93c85
Mario4CE <m.cerdas@estudiantec.cr>
2025-03-16 22:35:19 -0600

    Cambios en la interfaz

--- Cliente-MPointers/Cliente-MPointers/Cliente.h
+++ Cliente-MPointers/Cliente-MPointers/Cliente.h
@@ -33,6 +33,8 @@ namespace ClienteMPointers {
         System::Windows::Forms::Button^ btnCliente;
         System::Windows::Forms::TextBox^ txtPeticion;
         System::Windows::Forms::Label^ lblRespuesta;
+    private: System::Windows::Forms::Label^ lblTitle;
+
 
         // Contenedor de componentes
         System::ComponentModel::Container^ components;
@@ -49,29 +51,34 @@ namespace ClienteMPointers {
             this->btnCliente = (gcnew System::Windows::Forms::Button());
             this->txtPeticion = (gcnew System::Windows::Forms::TextBox());
             this->lblRespuesta = (gcnew System::Windows::Forms::Label());
+            this->lblTitle = (gcnew System::Windows::Forms::Label());
             this->SuspendLayout();
             // 
             // btnCliente
             // 
+            this->btnCliente->BackColor = System::Drawing::SystemColors::InactiveCaption;
             this->btnCliente->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
                 static_cast<System::Byte>(0)));
-            this->btnCliente->Location = System::Drawing::Point(262, 12);
+            this->btnCliente->Location = System::Drawing::Point(364, 117);
             this->btnCliente->Name = L"btnCliente";
             this->btnCliente->Size = System::Drawing::Size(224, 80);
             this->btnCliente->TabIndex = 0;
             this->btnCliente->Text = L"Enviar Petición";
-            this->btnCliente->UseVisualStyleBackColor = true;
+            this->btnCliente->UseVisualStyleBackColor = false;
             this->btnCliente->Click += gcnew System::EventHandler(this, &Cliente::btnCliente_Click);
             // 
             // txtPeticion
             // 
-            this->txtPeticion->Location = System::Drawing::Point(50, 100);
+            this->txtPeticion->Font = (gcnew System::Drawing::Font(L"Arial", 27.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+                static_cast<System::Byte>(0)));
+            this->txtPeticion->Location = System::Drawing::Point(102, 87);
             this->txtPeticion->Name = L"txtPeticion";
-            this->txtPeticion->Size = System::Drawing::Size(200, 20);
+            this->txtPeticion->Size = System::Drawing::Size(200, 50);
             this->txtPeticion->TabIndex = 1;
             // 
             // lblRespuesta
             // 
+            this->lblRespuesta->BackColor = System::Drawing::Color::LightGray;
             this->lblRespuesta->Font = (gcnew System::Drawing::Font(L"Arial Narrow", 21.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
                 static_cast<System::Byte>(0)));
             this->lblRespuesta->Location = System::Drawing::Point(50, 150);
@@ -80,11 +87,24 @@ namespace ClienteMPointers {
             this->lblRespuesta->TabIndex = 2;
             this->lblRespuesta->Text = L"Respuesta del servidor aparecerá aquí.";
             // 
+            // lblTitle
+            // 
+            this->lblTitle->AutoSize = true;
+            this->lblTitle->BackColor = System::Drawing::SystemColors::InactiveCaption;
+            this->lblTitle->Font = (gcnew System::Drawing::Font(L"Arial", 27.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+                static_cast<System::Byte>(0)));
+            this->lblTitle->Location = System::Drawing::Point(199, 20);
+            this->lblTitle->Name = L"lblTitle";
+            this->lblTitle->Size = System::Drawing::Size(379, 42);
+            this->lblTitle->TabIndex = 3;
+            this->lblTitle->Text = L"Servidor de MPointers";
+            // 
             // Cliente
             // 
             this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
             this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
             this->ClientSize = System::Drawing::Size(771, 374);
+            this->Controls->Add(this->lblTitle);
             this->Controls->Add(this->btnCliente);
             this->Controls->Add(this->txtPeticion);
             this->Controls->Add(this->lblRespuesta);
